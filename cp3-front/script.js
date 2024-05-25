5206a439dc0f9b6fd79f5ca70fcc9db2c4bbe92f
David Rapeckman Gomes de Souza <161530453+David-Rapeckman@users.noreply.github.com>
2024-05-24 22:57:56 -0300

    Add files via upload
    cp3

---
+++ cp3-front/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginForm');
+    const errorText = document.getElementById('errorText');
+    const logoutBtn = document.getElementById('logoutBtn');
+    const userDataDiv = document.getElementById('userData');
+  
+    // Function to handle login form submission
+    function handleLogin(event) {
+      event.preventDefault();
+      const email = loginForm.email.value;
+      const password = loginForm.password.value;
+  
+      if (email === 'usuario@email.com' && password === 'senha123') {
+        const userData = { email };
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+        window.location.href = 'index.html';
+      } else {
+        errorText.textContent = 'Email ou senha inválidos';
+        setTimeout(() => {
+          errorText.textContent = '';
+        }, 5000);
+      }
+    }
+  
+    // Function to handle logout button click
+    function handleLogout() {
+      sessionStorage.removeItem('userData');
+      window.location.href = 'login.html';
+    }
+  
+    // Function to display user data
+    function displayUserData() {
+      const userDataJSON = sessionStorage.getItem('userData');
+      if (userDataJSON) {
+        const userData = JSON.parse(userDataJSON);
+        userDataDiv.textContent = `Email: ${userData.email}`;
+      } else {
+        window.location.href = 'login.html';
+      }
+    }
+  
+    // Add event listeners
+    loginForm.addEventListener('submit', handleLogin);
+    logoutBtn.addEventListener('click', handleLogout);
+  
+    // Display user data
+    displayUserData();
+  });
